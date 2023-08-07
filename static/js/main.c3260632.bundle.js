b29986b261c88505e1c855baa89f70dd3b302921
铁蛋 <1543764041@qq.com>
2023-08-07 10:16:27 +0800

    Updates

---
+++ static/js/main.c3260632.bundle.js
@@ -0,0 +1,56 @@
+"use strict";
+(self["webpackChunkmmm"] = self["webpackChunkmmm"] || []).push([["main"],{
+
+/***/ 284:
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js
+var runtime_dom_esm_bundler = __webpack_require__(963);
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(252);
+;// CONCATENATED MODULE: ./src/App.jsx
+
+
+/* harmony default export */ var App = ((0,runtime_core_esm_bundler/* defineComponent */.aZ)({
+  render() {
+    const containerStyle = {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh"
+    };
+    return (0,runtime_core_esm_bundler/* createVNode */.Wm)("div", {
+      "style": containerStyle
+    }, [(0,runtime_core_esm_bundler/* createVNode */.Wm)("h1", {
+      "style": {
+        textAlign: "center",
+        margin: "auto"
+      }
+    }, [(0,runtime_core_esm_bundler/* createVNode */.Wm)("div", null, [(0,runtime_core_esm_bundler/* createTextVNode */.Uk)("\u6B22\u8FCE\u8BBF\u95EE")]), (0,runtime_core_esm_bundler/* createVNode */.Wm)("div", {
+      "style": {
+        marginBottom: "10px"
+      }
+    }, [(0,runtime_core_esm_bundler/* createVNode */.Wm)("a", {
+      "href": "https://juejin.cn/user/3782764966460398/posts"
+    }, [(0,runtime_core_esm_bundler/* createTextVNode */.Uk)("\u6398\u91D1\u5730\u5740")])]), (0,runtime_core_esm_bundler/* createVNode */.Wm)("div", null, [(0,runtime_core_esm_bundler/* createVNode */.Wm)("a", {
+      "href": "https://github.com/xun082/react-cli"
+    }, [(0,runtime_core_esm_bundler/* createTextVNode */.Uk)("Github\u5730\u5740")])])])]);
+  }
+}));
+;// CONCATENATED MODULE: ./src/main.js
+
+
+
+(0,runtime_dom_esm_bundler/* createApp */.ri)(App).mount("#app");
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["vendors-37dcf926","vendors-17c34557"], function() { return __webpack_exec__(284); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
